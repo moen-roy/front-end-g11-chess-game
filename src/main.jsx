7f08184bd4035adf801d30f2b4f1822d608b5b89
moen <moenr49@gmail.com>
2025-07-24 11:42:17 +0300

    importing bishop

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import { createRoot } from 'react-dom/client'
 import BoardComponent from './components/BoardComponents'
 import PawnComponent from './components/Pawn'
 import RookComponent from './components/Rook'
-import BishopComponent from './/components/BishopComponents'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
       <PawnComponent/>
