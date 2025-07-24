74d7fe1159daf8066712b894755c4a80c465f26a
moen <moenr49@gmail.com>
2025-07-24 11:59:08 +0300

    debuging: comiting the marge

--- src/main.jsx
+++ src/main.jsx
@@ -5,23 +5,17 @@ import { createRoot } from 'react-dom/client'
 import BoardComponent from './components/BoardComponents'
 import PawnComponent from './components/Pawn'
 import RookComponent from './components/Rook'
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+createRoot(document.getElementById('root')).render
+  (
+    <StrictMode>
       <PawnComponent/>
       <RookComponent />     
       
-//import BoardComponent from './components/BoardComponents';
-
-import BishopComponent from './components/Bishop';
-
-import PawnComponent from './components/Pawn';
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <PawnComponent />
       <BishopComponent />
   </StrictMode>,
-)
+  )
+
 // git pull origin 
 // git merge origin/animevite
 // git pull --no-rebase origin branch-name
