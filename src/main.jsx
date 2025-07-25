5a8edaa40d104cdf92a8b4fc9e41ad037bed8727
Euphraith <maganjoeuphraith256@gmail.com>
2025-07-25 12:18:57 +0300

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client';
+import Homepage from './components/Homepage';
 import QueenComponent from './components/QueenComponent';
 
 
-import BoardComponent from './components/BoardComponents'
+//import BoardComponent from './components/BoardComponents'
 
 import PawnComponent from './components/Pawn'
+import BishopComponent from './components/Bishop';
+//import Homepage from './components/Homepage';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Homepage />
     <PawnComponent/>
-        <BoardComponent></BoardComponent>
+      <QueenComponent />
+      <BishopComponent />
       
   </StrictMode>,
 )
